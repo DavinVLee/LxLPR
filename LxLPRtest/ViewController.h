b055af2b3711849bcec7574115483aa4742a4680
DavinVLee <970969044@qq.com>
2017-02-20 23:45:58 +0800

    firstCommit

---
+++ LxLPRtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LxLPRtest
+//
+//  Created by 李翔 on 17/2/20.
+//  Copyright © 2017年 李翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LxLPRtest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LxLPRtest
+//
+//  Created by 李翔 on 17/2/20.
+//  Copyright © 2017年 李翔. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
